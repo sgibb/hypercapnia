791b6c8dcfcb94463bcc751bfdc09a58798931c7
Sebastian Gibb <mail@sebastiangibb.de>
2021-10-27 14:49:56 +0200

    feat: add ianes data set

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Inhalational Anesthetics
+#'
+#' A dataset of physical properties of the some inhalational anesthetics.
+#'
+#' @format A `data.frame` with 3 rows and 7 columns:
+#' \describe{
+#' \item{Name}{`character`, name.}
+#' \item{MAC}{`double`, minimum alveolar concentration.}
+#' \item{MW}{`double`, molecular weight.}
+#' \item{Density}{`double`, density.}
+#' \item{GWP20}{`double`, global warming potential for 20 years.}
+#' \item{Unit}{`numeric`, mili litre per unit/bottle.}
+#' \item{Color}{`character`, color code.}
+#' }
+#'
+#' @references
+#' Ryan, Susan M., and Claus J. Nielsen.
+#' "Global warming potential of inhaled anesthetics: application to clinical use."
+#' Anesthesia & Analgesia 111.1 (2010): 92-98.
+#' \doi{10.1213/ane.0b013e3181e058d7}
+"ianes"
---
+++ data-raw/inhalational-anesthetics.R
@@ -0,0 +1,2 @@
+ianes <- read.csv("inhalational-anesthetics.csv")
+save(ianes, file = file.path("..", "data", "ianes.RData"))
